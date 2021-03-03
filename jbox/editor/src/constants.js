aa753972f304fe602e055faeeed4dd81a50917ad
Maciej Kozłowski <mjp.kozlowski@gmail.com>
2021-03-03 09:27:41 +0100

    Replace treblers preview with jbox

---
+++ jbox/editor/src/constants.js
@@ -0,0 +1,39 @@
+ const DRAW = "DRAW";
+
+ const ERASE = "ERASE";
+
+ const SPRITE = "SPRITE";
+
+ const TILEMAP = "TILEMAP";
+
+ const LOCAL_STORAGE_KEY = "JBOX_APP_DATA";
+
+ const TILE_SIZE = 8;
+
+ const MAX_SPRITES = 64;
+
+ const MAX_TILEMAP_SCREENS = 64;
+
+ const TILEMAP_SIZE = 16;
+
+ const MAX_SOUNDS = 32;
+
+ const SOUND_SAMPLE_COUNT = 32;
+
+ const FADE_IN = "fade-in";
+
+ const FADE_OUT = "fade-out";
+
+ const VIBRATO = "vibrato";
+
+ const SLIDE = "slide";
+
+ const middleC = 440 * Math.pow(Math.pow(2, 1 / 12), -9);
+
+ const buttonActiveClass = "btn-active";
+
+ const sectionActiveClass = "section-active";
+
+ const PEN = "PEN";
+
+ const FILL = "FILL";
